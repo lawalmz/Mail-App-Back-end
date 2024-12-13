696dde1f29b895453ded0c9b79d782c7ecfcd254
Lawal m Lawal <lawalmlawal@Emzels-MacBook-Air.local>
2024-12-13 11:00:45 +0100

    final

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 
 app.use(cors({
-  origin: ['http://localhost:3000',process.env.FFRONT_END],
+  origin: "*",
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
